c1e590fff7358721809fdce54c92e7a7f6a7e2e9
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-10 16:55:00 -0700

    Bootstrapping code generation

--- src/objclj/codegen.clj
+++ src/objclj/codegen.clj
@@ -1,1 +1,36 @@
-(ns objclj.codegen)
+(ns objclj.codegen
+  (:use [clojure.core.match :only [match]]))
+
+;;;
+;;; Objective-C ASTs and generation
+;;;
+
+(def bool-literal)
+
+(defn objc [ast]
+  "Translates an Objective-C AST into a string of Objective-C code"
+  (match ast
+         [bool-literal true] "YES"
+         [bool-literal false] "NO"
+         _ nil))
+
+;;;
+;;; Translating forms to Objective-C
+;;;
+
+(defn gen-form [form]
+  "Generates an Objective-C AST from a Clojure form"
+  (match form
+         true [bool-literal true]
+         false [bool-literal false]
+
+         _ nil))
+
+;;;
+;;; API
+;;;
+
+(defn codegen [& forms]
+  "Generates a string of Objective-C code from Clojure forms"
+  (doall
+    (map #(objc (gen-form %)) forms)))
--- src/objclj/repl.clj
+++ src/objclj/repl.clj
@@ -1,17 +1,21 @@
 (ns objclj.repl
   (:gen-class)
-  (:use objclj.codegen))
+  (:use [objclj.codegen :only [codegen]]))
 
 (defn -main [& args]
   (loop []
     (print "=> ")
     (flush)
+
+    ;; (recur) can't appear within (try ...), so we have the latter return a boolean value indicating whether we should loop
     (if
       (try
         ; TODO: handle end-of-line exceptions from (read), which should terminate the REPL
-        (println (eval (read)))
+        (println (codegen (read)))
         true
+
         (catch Exception e
           (println "*** Caught exception" e)
           true))
+
       (recur))))
