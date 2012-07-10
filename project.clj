7c762ff04da330fcad18d91ba344cbe0e3941fde
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-10 16:17:20 -0700

    Added dependency on core.match

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject objclj "0.1.0-SNAPSHOT"
   :description "Clojure to Objective-C compiler"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"], [org.clojure/core.match "0.2.0-alpha10"]])
