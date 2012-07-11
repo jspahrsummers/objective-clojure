9e34fda5b0d40f7f8ca6acfa678f411ffcfdd8c2
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-11 16:31:26 -0700

    Replaced richard-lyman/amotoen with the amazing van-clj/zetta-parser

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "https://github.com/jspahrsummers/objective-clojure"
   :license "Public Domain"
   :main objclj.repl
-  :dependencies [[org.clojure/clojure "1.4.0"] [org.clojure/core.match "0.2.0-alpha10"]]
+  :dependencies [[org.clojure/clojure "1.4.0"] [org.clojure/core.match "0.2.0-alpha10"] [org.van-clj/zetta-parser "0.0.3"]]
   :plugins [[lein-git-deps "0.0.1-SNAPSHOT"]]
-  :git-dependencies [["git://github.com/richard-lyman/amotoen.git", "50da08b6d45149112d69378d6751dfeb0657b339"]]
-  :source-paths ["src" ".lein-git-deps/amotoen/src/"])
+  :source-paths ["src"])
--- src/objclj/reader.clj
+++ src/objclj/reader.clj
@@ -1,72 +1,40 @@
 (ns objclj.reader
-  (:use com.lithinos.amotoen.core))
+  ; Zetta defines many symbols that conflict with builtins
+  (:refer-clojure :exclude [ensure, get, char, take-while, take, replicate])
+  (:use clojure.algo.monads)
+  (:use [zetta.core :exclude [parse]])
+  (:use [zetta.parser.seq :exclude [whitespace, skip-whitespaces]])
+  (:use zetta.combinators))
 
 ;;;
-;;; AST cleanup
+;;; Parsers
 ;;;
 
-(defmulti clean-ast
-  "Turns an Amotoen AST into a more representative Clojure data structure"
-  #(first (keys %)))
+(defn is-whitespace [c]
+  (or (= c \,) (Character/isWhitespace #^java.lang.Character c)))
 
-(defmacro make-clean-ast [key val & exprs]
-  "Defines a method of multimethod clean-ast, matching against the given key, and destructuring to the given value"
-  `(defmethod clean-ast ~key [{ ~val ~key }]
-     ~@exprs))
+(def whitespace
+  (satisfy? is-whitespace))
 
-(make-clean-ast :symbol [x xs]
-    (symbol (str x (doall xs))))
+(def skip-whitespaces
+  (skip-many whitespace))
 
-(make-clean-ast :nil _
-    nil)
+(def nil-literal
+  (<* (always nil)
+      (string "nil")))
 
-(make-clean-ast :true _
-    true)
+(def true-literal
+  (<* (always true)
+      (string "true")))
 
-(make-clean-ast :false _
-    false)
+(def false-literal
+  (<* (always false)
+      (string "false")))
 
-(make-clean-ast :list [_ _ forms _ _]
-    nil)
-
-(make-clean-ast :form [_ f]
-    (clean-ast f))
-
-;;;
-;;; Parsing
-;;;
-
-;; The following definitions are not part of the PEG grammar itself, so as to not appear in generated ASTs
-
-; Allow everything but these characters to begin a symbol
-(def symbol-start (lpegs '% "0123456789(){}#\"';@^`~\\"))
-
-; Allow even more characters within a symbol
-(def symbol-char (lpegs '% "(){}\"';@`~"))
-
-; Single whitespace characters (including commas)
-(def whitespace-char (lpegs '| "\n\r\t ,"))
-
-; Semicolon followed by any number of non-newline characters
-(def line-comment [ \; '(* (% \newline )) ])
-
-(def grammar
-  "PEG grammar for Clojure"
-  {
-    :_* (list '* whitespace-char line-comment)
-    :nil (pegs "nil")
-    :true (pegs "true")
-    :false (pegs "false")
-    :symbol [ symbol-start (list '* symbol-char) ]
-    :list [ \( '(* :form) :_* \) ]
-    :form [ :_* '(| :nil :true :false :list :symbol ) ]
-  })
-
-(validate grammar)
+(def parser
+  (>> skip-whitespaces
+      (choice [nil-literal true-literal false-literal])))
 
 (defn parse [str]
   "Parses a string of Clojure code into an AST"
-  (let [ast (pegasus :form grammar (gen-ps str))]
-    ; TODO: For debugging only
-    (println ast)
-    (clean-ast ast)))
+  (parse-once parser str))
