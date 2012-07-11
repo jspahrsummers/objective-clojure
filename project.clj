91363cb46963f7fb7aabcd2849039dccf574dbb1
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-11 13:58:35 -0700

    Added richard-lyman/amotoen to project

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
   :main objclj.repl
   :dependencies [[org.clojure/clojure "1.4.0"] [org.clojure/core.match "0.2.0-alpha10"]]
   :plugins [[lein-git-deps "0.0.1-SNAPSHOT"]]
-  :source-paths ["src"])
+  :git-dependencies [["git://github.com/richard-lyman/amotoen.git", "50da08b6d45149112d69378d6751dfeb0657b339"]]
+  :source-paths ["src" ".lein-git-deps/amotoen/src/"])
