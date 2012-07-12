d4782a448f87ff66449e330bfd95521b4f45efe8
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-12 14:26:03 -0700

    Unit tests for util/append

--- src/objclj/util.clj
+++ src/objclj/util.clj
@@ -1,6 +1,14 @@
-(ns objclj.util)
+(ns objclj.util
+  (:use clojure.test)
+  (:use objclj.test))
 
-(defn append
+(with-test
+  (defn append
     "Appends zero or more elements to a collection."
     [coll & elems]
     (concat coll elems))
+
+  (is= [:foo] (append [:foo]))
+  (is= [:foo :bar] (append [:foo] :bar))
+  (is= [:foo :bar :fuzz] (append [:foo] :bar :fuzz))
+  (is= [:foo :bar :fuzz] (append [:foo :bar] :fuzz)))
