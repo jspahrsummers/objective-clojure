c492bf955387546583371d8c30803443c6794251
joshaber <joshaber@gmail.com>
2012-07-12 00:15:31 -0400

    removed todo comment

--- src/objclj/repl.clj
+++ src/objclj/repl.clj
@@ -12,7 +12,6 @@
     ;; (recur) can't appear within (try ...), so we have the latter return a boolean value indicating whether we should loop
     (if
       (try
-        ; TODO: handle end-of-line, which should terminate the REPL
         (let [input (read-line)]
           (if (= input nil)
             (do
