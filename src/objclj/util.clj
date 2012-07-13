5cd459c67d551e6b1b150d0b7c40ba9c6475c3e5
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-12 18:12:19 -0700

    Added util/index-of, to get the index of an item in a sequence

--- src/objclj/util.clj
+++ src/objclj/util.clj
@@ -13,6 +13,20 @@
   (is= [:foo :bar :fuzz] (append [:foo] :bar :fuzz))
   (is= [:foo :bar :fuzz] (append [:foo :bar] :fuzz)))
 
+(with-test
+  (defn index-of
+    "Returns the index of item in sequence s, or nil if it could not be found."
+    [item s]
+    (let [matches (filter #(= (second %) item) (keep-indexed list s))
+          first-match (first matches)]
+      (first first-match)))
+
+  (is= 0 (index-of :foo [:foo :bar :fuzz :buzz]))
+  (is= 2 (index-of :fuzz [:foo :bar :fuzz :buzz]))
+  (is= 2 (index-of :fuzz (list :foo :bar :fuzz :buzz)))
+  (is= 1 (index-of \o "foo"))
+  (is= nil (index-of :not-here [:foo :bar :fuzz :buzz])))
+
 (defmulti collect-type
   "Given a collection interface and a sequence, returns a concrete collection of that type containing the items in the sequence.
   If the collection type is a map, the sequence should follow the form of [k v k2 v2 k3 v3 ...] and must have an even number of items."
