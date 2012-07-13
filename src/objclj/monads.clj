7c613d300b145583d517a39cf329820f0ee43421
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-13 00:08:36 -0700

    Got rid of objclj.monads/domonad for the time being, since it's not as flexible as the one in algo.monads

--- src/objclj/monads.clj
+++ src/objclj/monads.clj
@@ -1,40 +1,14 @@
 (ns objclj.monads
-  (:use [clojure.algo.monads :rename { domonad m-domonad } :exclude [fetch-val]])
+  (:use [clojure.algo.monads :exclude [fetch-val]])
   (:use clojure.test)
   (:use objclj.test))
-
-(defmacro domonad
-  "Within a with-monad block, allows Haskell-esque do notation. This macro takes any number of expressions, and rewrites any of the form \"(binding <- monadic-value)\" into the equivalent clojure.algo.monads/domonad structure.
-
-  The last expression given is returned, identically to the final argument of clojure.algo.monads/domonad. <- may not appear in the last expression."
-  [& exprs]
-
-  ; TODO: improve this to not require parentheses around (x <- y)
-  (let [monad-exprs (map #(if (and (list? %) (= (second %) '<-))
-                              (cons (first %) (drop 2 %))
-                              (list '_ %))
-                    (drop-last exprs))
-        flat-exprs (vec (reduce concat monad-exprs))]
-    (list `m-domonad flat-exprs (last exprs))))
-
-(deftest test-domonad
-  (with-monad (writer-m "")
-    (is= [3 "foobar"] (domonad
-                        (x <- (m-result 1))
-                        (write "foo")
-                        (y <- (m-result 2))
-                        (write "bar")
-                        (+ x y)))))
 
 (with-test
   (defn fetch-val
     "A rewritten version of clojure.algo.monads/fetch-val that does not depend on the key being any particular type."
     [key]
-    (with-monad state-m
-      (domonad
-        (s <- (fetch-state))
-        (get s key))))
+    (domonad state-m [s (fetch-state)] (get s key)))
 
-  (let [st (with-monad state-m
-             (domonad (x <- (fetch-val :foo)) (str x)))]
+  (let [st (domonad state-m [x (fetch-val :foo)]
+             (str x))]
     (is= [":bar" { :foo :bar }] (st { :foo :bar }))))
